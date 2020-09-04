e2c8d124e299e3877c2b94aa232f3efc4dcf5d76
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-09-04 10:03:59 +0530

    hero section update and typewriter effect added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var options = {
+	strings: ['Ideate', 'Innovate', 'Create', 'Hack', 'Some', 'More', 'Random', 'Buzz', 'Words'],
+	typeSpeed: 100,
+	loop: true,
+	smartBackspace: false,
+	showCursor: true,
+	cursorChar: '_',
+	backSpeed: 40,
+	backDelay: 2000,
+};
+var typed = new Typed('.title', options);
